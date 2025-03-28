1841885f52efc0259ea6a4ed58edbbc8b8ec1aa6
Christopher Greening <chris@christophergreening.com>
2025-03-27 20:46:56 -0400

    ADD: added tests

--- tests/test_new.py
+++ tests/test_new.py
@@ -35,19 +35,3 @@ def test_scaffold_basic_project(temp_project_dir, monkeypatch):
     # print(os.listdir(temp_project_dir))
     assert src_path.exists() and src_path.is_dir(), f"{temp_project_dir}/ directory should exist"
     assert any(p.suffix == ".py" for p in src_path.rglob("*")), f"No .py files found in {temp_project_dir}/"
-
-# def test_project_context_used(temp_project_dir):
-#     config = {
-#         "project_name": "Overridden Project",
-#         "project_slug": "overridden_project"
-#     }
-#     (temp_project_dir / "greening.json").write_text(json.dumps(config))
-#     greening_new.new(str(temp_project_dir))
-#     assert (temp_project_dir / ".git").exists()
-#     assert (temp_project_dir / "README.md").exists()
-
-# def test_scaffold_creates_expected_files(temp_project_dir):
-#     greening_new.new(str(temp_project_dir))
-#     expected = ["README.md", "pyproject.toml", "src"]
-#     for name in expected:
-#         assert (temp_project_dir / name).exists()
