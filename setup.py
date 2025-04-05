998c5dd9a407817b17daca0cb103ca10ca9582e3
chris-greening <chrisgreening3@gmail.com>
2025-04-05 13:06:11 -0400

    ADD: added missing install_requires

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version="0.4.3",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        "cookiecutter",
+        "requests"
+    ],
     entry_points={
         "console_scripts": [
             "greening = greening.cli:main"
